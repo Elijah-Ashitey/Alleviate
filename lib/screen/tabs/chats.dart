51578617dd5f9c52df7569542c6022518ef30a5e
El_Eli <elijah.ashitey@unionsg.com>
2020-03-10 15:29:18 +0000

    two tabs completed

--- lib/screen/tabs/chats.dart
+++ lib/screen/tabs/chats.dart
@@ -1,3 +1,6 @@
+import 'package:alleviate/data/chatData.dart';
+import 'package:alleviate/widget/appBar.dart';
+import 'package:alleviate/widget/chatWidget.dart';
 import 'package:flutter/material.dart';
 
 class Chat extends StatefulWidget {
@@ -9,156 +12,30 @@ class _ChatState extends State<Chat> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: Text(""),
-        title: Text(
-          "Home",
-          style: TextStyle(color: Colors.blue),
-        ),
-        // centerTitle: true,
-        elevation: 0,
-        backgroundColor: Colors.transparent,
-        actions: <Widget>[
-          IconButton(
-              icon: Icon(
-                Icons.notification_important,
-                color: Colors.blue,
-              ),
-              onPressed: () {}),
-          IconButton(
-              icon: Icon(
-                Icons.more_vert,
-                color: Colors.blue,
-              ),
-              onPressed: () {})
-        ],
-      ),
+      appBar: appBar("Home"),
       backgroundColor: Color(0xffe3f2fd),
-      body: ListView(
+      body: Column(
         // crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
-          Center(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.blue,
-                borderRadius: BorderRadius.all(Radius.circular(30)),
-              ),
-              height: MediaQuery.of(context).size.height * 0.05,
-              width: MediaQuery.of(context).size.width * 0.8,
-              child: TextField(
-                textAlign: TextAlign.center,
-
-                decoration: InputDecoration(
-                  hintText: "Whats on your mind?",
-                  border: InputBorder.none,
-                  hintStyle: TextStyle(
-                    // fontSize: 14,
-                    color: Colors.white,
-                  ),
-                ),
-                // controller: _accountNumber,
-              ),
+          whatOnYourMind(context),
+          Expanded(
+              child: Container(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return ChatWidget(
+                  image: chats[index].image,
+                  person: chats[index].person,
+                  time: chats[index].time,
+                  chat: chats[index].chat,
+                  messages: chats[index].messages,
+                  messImage: chats[index].messImage,
+                );
+              },
+              itemCount: chats.length,
             ),
-          ),
-          // SizedBox(height: 10),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              // decoration: BoxDecoration(
-              //   color: Colors.blue,
-              //   borderRadius: BorderRadius.all(Radius.circular(30)),
-              // ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    decoration: BoxDecoration(
-                      color: Colors.blue,
-                      borderRadius: BorderRadius.all(Radius.circular(30)),
-                    ),
-                    child: Padding(
-                      padding: const EdgeInsets.all(8.0),
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                              child: Column(
-                            children: <Widget>[
-                              Row(
-                                children: <Widget>[
-                                  CircleAvatar(
-                                    backgroundColor: Color(0xffe3f2fd),
-                                    child: Icon(Icons.person),
-                                  ),
-                                ],
-                              )
-                            ],
-                          )
-
-                              /*  Row(
-                              children: <Widget>[
-                                CircleAvatar(
-                                  backgroundColor: Color(0xffe3f2fd),
-                                  child: Icon(Icons.person),
-                                ),
-                                SizedBox(
-                                  width:
-                                      MediaQuery.of(context).size.width * 0.02,
-                                ),
-                                // time()
-                                Row(
-                                  children: <Widget>[
-                                    Text(
-                                      "Japan",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                    Text(
-                                      "just now",
-                                      textAlign: TextAlign.end,
-                                      style: TextStyle(color: Colors.white),
-                                    ),
-                                  ],
-                                ),
-                                Container(
-                                  color: Colors.white,
-                                  width: 110,
-                                  height: 2,
-                                ),
-                                // Text(
-                                //   "Japan",
-                                //   style: TextStyle(color: Colors.white),
-                                // ),
-                                // Expanded(
-                                //   child: Text(
-                                //     "just now",
-                                //     textAlign: TextAlign.end,
-                                //     style: TextStyle(color: Colors.white),
-                                //   ),
-                                // ),
-                              ],
-                            ),*/
-                              ),
-                        ],
-                      ),
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 20.0),
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.message,
-                          color: Colors.blue,
-                        ),
-                        Text(
-                          "5",
-                          style: TextStyle(color: Colors.blue),
-                        ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
-            ),
-          )
+          ))
+          // ChatWidget(),
+          // ChatWidget(),
         ],
       ),
     );
@@ -180,4 +57,30 @@ Widget time() {
       ),
     ],
   ));
+}
+
+Widget whatOnYourMind(BuildContext context) {
+  return Center(
+    child: Container(
+      decoration: BoxDecoration(
+        color: Colors.blue,
+        borderRadius: BorderRadius.all(Radius.circular(30)),
+      ),
+      height: MediaQuery.of(context).size.height * 0.05,
+      width: MediaQuery.of(context).size.width * 0.8,
+      child: TextField(
+        textAlign: TextAlign.center,
+
+        decoration: InputDecoration(
+          hintText: "Whats on your mind?",
+          border: InputBorder.none,
+          hintStyle: TextStyle(
+            // fontSize: 14,
+            color: Colors.white,
+          ),
+        ),
+        // controller: _accountNumber,
+      ),
+    ),
+  );
 }
--- lib/screen/tabs/happy.dart
+++ lib/screen/tabs/happy.dart
@@ -1,3 +1,4 @@
+import 'package:alleviate/widget/appBar.dart';
 import 'package:flutter/material.dart';
 
 class Happy extends StatefulWidget {
@@ -9,10 +10,44 @@ class _HappyState extends State<Happy> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blueAccent,
-      body: Center(
-        child: Text("Happy goes here"),
-      ),
-    );
+        appBar: appBar("Happy"),
+        backgroundColor: Color(0xffe3f2fd),
+        body: GridView.count(
+          crossAxisCount: 2,
+          children: <Widget>[
+            tabs(() {}, Icons.movie, "MOVIES"),
+            tabs(() {}, Icons.music_note, "MUSIC"),
+            tabs(() {}, Icons.gamepad, "GAMES"),
+            tabs(() {}, Icons.person, "EXERCISE"),
+            tabs(() {}, Icons.book, "MOTIVATIONAL QUOTES"),
+            tabs(() {}, Icons.favorite, "FAVOURITE"),
+          ],
+        ));
   }
+}
+
+Widget tabs(Function function, IconData icon, String text) {
+  return GestureDetector(
+    onTap: function,
+    child: Container(
+        child: Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Icon(
+          icon,
+          color: Colors.blue,
+          size: 55,
+        ),
+        Text(
+          text,
+          textAlign: TextAlign.center,
+          style: TextStyle(
+            color: Colors.blue,
+            fontSize: 20,
+          ),
+        ),
+      ],
+    )),
+  );
 }
